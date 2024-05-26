b7217b4ff3f16ae6fff7fbbf60a8c87b6dd9d25b
cayirmedine <htc.mdn.2010@gmail.com>
2024-05-26 23:38:46 +0300

    name definition removed from index paths

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views #use a file from same directory
 
 urlpatterns = [
-    path("", views.index, name='index'),
+    path("", views.index),
     path("index", views.index, name='index'),
-    path("detail", views.detail, name='detail'),
     #path('<category>', views.getProductByCategory) #dynamic path
     path('<int:category_id>', views.getProductByCategoryId), #accepts only int value as a parameter
     path('<str:category>', views.getProductByCategory, name="products_by_category") #accpets only str value as a parameter, but everything can be count as a str
