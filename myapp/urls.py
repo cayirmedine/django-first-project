4b1ebbb923eaa95cd131a8de5bb7216cdf153bfc
cayirmedine <htc.mdn.2010@gmail.com>
2024-05-26 23:10:07 +0300

    A dictionary added for getProductByCategoryId

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,10 +4,9 @@ from . import views #use a file from same directory
 urlpatterns = [
     path("", views.index, name='index'),
     path("index", views.index, name='index'),
-    path("list", views.list, name='list'),
     path("detail", views.detail, name='detail'),
     #path('<category>', views.getProductByCategory) #dynamic path
-    path('<int:category>', views.getProductByCategoryId), #accepts only int value as a parameter
+    path('<int:category_id>', views.getProductByCategoryId), #accepts only int value as a parameter
     path('<str:category>', views.getProductByCategory) #accpets only str value as a parameter, but everything can be count as a str
     #if str parameter define before int, int parameters never go into getProductByCategoryId
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,20 +1,23 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.shortcuts import render, redirect
+from django.http.response import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 
 # Create your views here.
+
+url_data = {
+    "computer": "Computer Category",
+    "phone": "Phone Category",
+    "electronic": "Electronic Category",
+}
 
 # http://localhost:8000
 def index(request):
     return HttpResponse("Hello World!")
 
-def list(request):
-    return HttpResponse("List")
-
 def detail(request):
     return HttpResponse("Detail")
 
 def getProductByCategory(request, category):
-    category_text = None
+    '''category_text = None
 
     if category == "computer":
         category_text = "Computer Category"
@@ -25,7 +28,24 @@ def getProductByCategory(request, category):
     else:
         category_text = "Wrong Category"
 
-    return HttpResponse(category_text)
+    return HttpResponse(category_text)'''
 
-def getProductByCategoryId(request, category):
-    return HttpResponse(category)
+    try:
+        category_text = url_data[category]
+        return HttpResponse(category_text)
+    except:
+        return HttpResponseNotFound("Wrong Category")
+
+def getProductByCategoryId(request, category_id):
+    #return HttpResponse(category)
+    #return HttpResponseRedirect("/products/computer") #redirect, HttpResponseRedirect("computer") works too
+    #return redirect("/products/computer")
+
+    category_list = list(url_data.keys())
+
+    if category_id > len(category_list):
+        return HttpResponseNotFound("Wrong Category")
+
+    redirect_text = category_list[category_id-1]
+
+    return redirect("/products/"+ redirect_text)
