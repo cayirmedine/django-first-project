cc7c83adc0d02ea8a18dd5f99233c7efe0ca713f
cayirmedine <htc.mdn.2010@gmail.com>
2024-05-29 14:47:29 +0300

    Template directory defined in settings.py

--- examples/settings.py
+++ examples/settings.py
@@ -56,7 +56,10 @@ ROOT_URLCONF = 'examples.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates",
+            # BASE_DIR / "templates" / "myapp" #you can define like that but this time, you have to define all app's directories
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
