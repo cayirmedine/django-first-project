a4c43b1f3c13a6a85be53cf832a40ba6465d59b8
cayirmedine <htc.mdn.2010@gmail.com>
2024-06-20 12:40:48 +0300

    Str constructor added to Product model

--- myapp/models.py
+++ myapp/models.py
@@ -6,3 +6,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=3)
     description = models.TextField()
     img_url = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name} {self.price}"
