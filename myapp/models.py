e861bf724742b81c4aa5ffc882015e857b2a0ff3
cayirmedine <htc.mdn.2010@gmail.com>
2024-06-20 12:58:01 +0300

    New attrs added to Product model

--- myapp/models.py
+++ myapp/models.py
@@ -6,6 +6,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=3)
     description = models.TextField()
     img_url = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True) #You can use null=True instead of default=True, if you want default null value
+    category = models.CharField(max_length=50, null=True) #blank means null string
 
     def __str__(self):
         return f"{self.name} {self.price}"
