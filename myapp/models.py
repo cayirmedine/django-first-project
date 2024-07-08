096680276ef2331c54b58f412f0d0fa7df44edc4
cayirmedine <htc.mdn.2010@gmail.com>
2024-07-08 16:24:32 +0300

    slug field added for Product model

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Product(models.Model):
@@ -8,6 +9,11 @@ class Product(models.Model):
     img_url = models.CharField(max_length=50)
     is_active = models.BooleanField(default=True) #You can use null=True instead of default=True, if you want default null value
     category = models.CharField(max_length=50, null=True) #blank means null string
+    slug = models.SlugField(default="", null=False) # null=False equals to not nullable
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(args, kwargs)
 
     def __str__(self):
         return f"{self.name} {self.price}"
