f9dd66cd382700a6f935d965d220c60e8c0915b7
cayirmedine <htc.mdn.2010@gmail.com>
2024-07-12 17:12:23 +0300

    Auto slug configuration added and category model introduced to admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "price", "is_active", "slug")
-    readonly_fields = ["slug"]
+    list_display = ("name", "price", "is_active", "slug", "category")
+    #readonly_fields = ["slug"]
+    prepopulated_fields = { "slug": ["name"] }
     list_display_links = ("name", "slug")
     list_filter = ("name", "price", "category")
     list_editable = ["is_active"]
@@ -11,3 +12,4 @@ class ProductAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Product, ProductAdmin) #introduce Product model to admin
+admin.site.register(Category)
