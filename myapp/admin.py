afe6d3a99e38becbe7eb4852af1901b9ff3a4951
cayirmedine <htc.mdn.2010@gmail.com>
2024-07-12 15:17:26 +0300

    Display changes added on Product model in admin page

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Product
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "is_active", "slug")
+    readonly_fields = ["slug"]
+    list_display_links = ("name", "slug")
+    list_filter = ("name", "price", "category")
+    list_editable = ["is_active"]
+    search_fields = ("name", "description")
+
 # Register your models here.
-admin.site.register(Product) #introduce Product model to admin
+admin.site.register(Product, ProductAdmin) #introduce Product model to admin
