4a761c537ae529d370f513cb44416861cfdf0e64
cayirmedine <htc.mdn.2010@gmail.com>
2024-07-15 17:41:57 +0300

    List product's categories field added to admin page

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,13 +2,23 @@ from django.contrib import admin
 from .models import Product, Category
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "price", "is_active", "slug")
+    list_display = ("name", "price", "is_active", "slug", "selected_categories")
     #readonly_fields = ["slug"]
     prepopulated_fields = { "slug": ["name"] }
     list_display_links = ("name", "slug")
-    list_filter = ("name", "price")
+    list_filter = ("name", "price", "categories")
     list_editable = ["is_active"]
     search_fields = ("name", "description")
+
+    def selected_categories(self, obj):
+        html = ""
+
+        categories = obj.categories.all()
+
+        for cat in categories:
+            html += cat.name + " "
+
+        return html
 
 # Register your models here.
 admin.site.register(Product, ProductAdmin) #introduce Product model to admin
