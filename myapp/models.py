872a0efd557b3ea43d3d596ed553f07a393c824a
cayirmedine <htc.mdn.2010@gmail.com>
2024-07-15 17:05:02 +0300

    OnetoMany relation changed to ManytoMany between product and category

--- myapp/models.py
+++ myapp/models.py
@@ -15,13 +15,14 @@ class Product(models.Model):
     img_url = models.CharField(max_length=50)
     is_active = models.BooleanField(default=True) #You can use null=True instead of default=True, if you want default null value
     #category = models.CharField(max_length=50, null=True) #blank means null string
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, related_name="products")
+    #category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, related_name="products")
     slug = models.SlugField(default="", null=False, db_index=True, unique=True, blank=True) 
     # null=False equals to not nullable
     #if you define primary_key=True then automatically defined as unique=True
     #if you use primary_key then id not created automatically
     #blank=True means you dont need to enter slug value in form
     #if you use editable=False, defined attr does not display in admin form 
+    categories = models.ManyToManyField(Category, related_name="products")
 
     def __str__(self):
         return f"{self.name} {self.price} {self.slug}"
