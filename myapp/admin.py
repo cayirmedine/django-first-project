df655a3c2faff32688aefa440199a1f2090adc22
cayirmedine <htc.mdn.2010@gmail.com>
2024-07-17 16:58:52 +0300

    Address and supplier model introduced to admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Address, Supplier
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "price", "is_active", "slug", "selected_categories")
@@ -23,3 +23,5 @@ class ProductAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Product, ProductAdmin) #introduce Product model to admin
 admin.site.register(Category)
+admin.site.register(Address)
+admin.site.register(Supplier)
