4adef688808d8cbe3a0c6240f048db57a9bc4dd0
cayirmedine <htc.mdn.2010@gmail.com>
2024-07-21 14:07:15 +0300

    l ist url path added and unnecessary paths removed

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,9 +5,10 @@ urlpatterns = [
     path("", views.index),
     path("index", views.index, name='index'),
     #path("<int:id>", views.details),
+    path("list", views.list),
     path("<slug:slug>", views.details, name="product-details"),
     #path('<category>', views.getProductByCategory) #dynamic path
-    path('<int:category_id>', views.getProductByCategoryId), #accepts only int value as a parameter
-    path('<str:category>', views.getProductByCategory, name="products_by_category") #accpets only str value as a parameter, but everything can be count as a str
+    #path('<int:category_id>', views.getProductByCategoryId), #accepts only int value as a parameter
+    #path('<str:category>', views.getProductByCategory, name="products_by_category"), #accpets only str value as a parameter, but everything can be count as a str
     #if str parameter define before int, int parameters never go into getProductByCategoryId
 ]
